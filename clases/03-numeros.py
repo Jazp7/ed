0d054ea31945788843c1f335168439b0fc0abf40
Jazp7 <aristideszelayax@gmail.com>
2026-07-28 12:03:08 -0600

    chore: Reorganizar el código y añadir clase 5

---
+++ clases/03-numeros.py
@@ -0,0 +1,23 @@
+print("Bienvenido@ " + input("Ingresa tu nombre ") + input("Ingresa tu apellido "))
+
+nombre = "Juan Pérez"
+edad = 32
+nacionalidad = "Cabo Verde"
+
+print(f"Hola, Yo soy: {nombre} tengo {str(edad)} y soy de {nacionalidad}")
+
+
+# Operadores matemáticos
+n1 = 5
+n2 = 10
+n3 = 15
+n4 = 20
+
+print("La suma de los números n1 y n2 es: " + str(n1 + n2))
+print("La resta de los números n4 y n3 es: " + str(n4 - n3))
+print("La multiplicación de los números n1 y n2 es: " + str(n1 * n2))
+print("La división de los números n4 y n3 es: " + str(n4 / n3))
+
+# Elevar a X potencia
+print("La elevación del número n1 a n2 potencia es: " + str(n1 ** n2))
+print("El número: " + str(n3) + " es par o impar: " + str(n3 % 2))
